8e746cc2f9a784ecb5b9675f8e3695b9700f186d
kareem5 <k_m52@hotmail.com>
2020-03-05 11:56:59 +0300

    Initial project setup

---
+++ SwiftyLibKareem/SwiftyLibKareem.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLibKareem.h
+//  SwiftyLibKareem
+//
+//  Created by Kareem Ahmed on 3/5/20.
+//  Copyright © 2020 Kareem Ahmed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibKareem.
+FOUNDATION_EXPORT double SwiftyLibKareemVersionNumber;
+
+//! Project version string for SwiftyLibKareem.
+FOUNDATION_EXPORT const unsigned char SwiftyLibKareemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibKareem/PublicHeader.h>
+
+
